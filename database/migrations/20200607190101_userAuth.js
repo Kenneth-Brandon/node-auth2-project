121db3bbab95586ef81933b900c04722609edcf8
unknown <ken.brandon@aol.com>
2020-06-07 19:09:37 -0700

    cleanup seed

---
+++ database/migrations/20200607190101_userAuth.js
@@ -0,0 +1,12 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('userAuth', (tbl) => {
+    tbl.increments('id');
+    tbl.string('username', 128).notNullable().unique().index();
+    tbl.string('password', 256).notNullable();
+    tbl.string('department', 256).notNullable();
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('userAuth');
+};
